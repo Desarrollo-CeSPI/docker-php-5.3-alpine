0f46ee615752a46f801ac4ffbedb8007e0c3cc23
Christian Rodriguez <chrodriguez@gmail.com>
2017-02-15 14:34:47 -0300

    updated build-all to allow push to repo

--- build-all.sh
+++ build-all.sh
@@ -2,10 +2,17 @@
 
 set -e 
 
-docker build --rm -t cespi/php-5.3 .
+docker build --rm -t cespi/php-5.3:cli .
 docker build --rm -t cespi/php-5.3:apache apache/
 docker build --rm -t cespi/php-5.3:fpm fpm/
 
-docker build --rm -t cespi/php-5.3:modules with-modules
+docker build --rm -t cespi/php-5.3:modules-cli with-modules
 docker build --rm -t cespi/php-5.3:modules-apache with-modules/apache
 docker build --rm -t cespi/php-5.3:modules-fpm with-modules/fpm
+
+if [ "$1" = "push" ]; then
+  for tag in cli apache fpm; do
+    docker push cespi/php-5.3:$tag
+    docker push cespi/php-5.3:modules-$tag
+  done
+fi
